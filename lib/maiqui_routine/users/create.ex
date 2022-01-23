fb8dd47cf7a79be9691449681c8588c09fa4b59d
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-23 08:37:43 -0300

    :recycle: add guard

--- lib/maiqui_routine/users/create.ex
+++ lib/maiqui_routine/users/create.ex
@@ -2,7 +2,7 @@ defmodule MaiquiRoutine.Users.Create do
   alias MaiquiRoutine.{User, Repo}
 
   @spec call(User.params()) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
-  def call(%{} = params) do
+  def call(params) when is_map(params) do
     %User{}
     |> User.changeset(params)
     |> Repo.insert()
