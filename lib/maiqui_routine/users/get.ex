a043e71c222964312691ed5793a9520e21c1a80d
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-23 08:34:36 -0300

    :recycle: add guard

--- lib/maiqui_routine/users/get.ex
+++ lib/maiqui_routine/users/get.ex
@@ -2,13 +2,13 @@ defmodule MaiquiRoutine.Users.Get do
   alias MaiquiRoutine.{User, Repo}
 
   @spec call(binary()) :: {:ok, Ecto.Schema.t()} | {:error, String.t()}
-  def call(id) do
+  def call(id) when is_binary(id) do
     id
     |> Ecto.UUID.cast()
     |> handle_cast()
   end
 
-  defp handle_cast({:ok, id}) do
+  defp handle_cast({:ok, id}) when is_binary(id) do
     case Repo.get(User, id) do
       nil -> {:error, "User not found"}
       user -> {:ok, user}
