dd459da946db734f11fe85154f85821e6c1b2171
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-26 19:16:46 -0300

    :memo: add doc

--- lib/maiqui_routine/users/create.ex
+++ lib/maiqui_routine/users/create.ex
@@ -1,6 +1,15 @@
 defmodule MaiquiRoutine.Users.Create do
   alias MaiquiRoutine.{User, Repo}
 
+  @doc """
+  ### Examples
+
+      iex> user = %{name: "Maiqui", email: "maiqui@gmail.com", password: "12345678"}
+
+      iex> MaiquiRoutine.Users.Create.call user
+      {:ok, %MaiquiRoutine.User{}}
+
+  """
   @spec call(User.params()) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
   def call(params) when is_map(params) do
     %User{}
