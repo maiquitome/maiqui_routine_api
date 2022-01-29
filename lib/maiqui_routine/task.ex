1cf40fec3b9ff102185dc7da4b7720609eea1298
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-28 21:38:54 -0300

    :sparkles: add weekdays field

--- lib/maiqui_routine/task.ex
+++ lib/maiqui_routine/task.ex
@@ -2,12 +2,33 @@ defmodule MaiquiRoutine.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @type params() :: %{
+          name: String.t(),
+          start_time: Time.t(),
+          end_time: Time.t(),
+          category_id: binary(),
+          user_id: binary(),
+          weekdays: [:sun | :tue | :mon | :web | :thu | :fri | :sat]
+        }
+
+  @required_fields [
+    :name,
+    :start_time,
+    :end_time,
+    :category_id,
+    :user_id,
+    :weekdays
+  ]
+
+  @fields_that_can_be_changed [] ++ @required_fields
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "tasks" do
     field :end_time, :time
     field :name, :string
     field :start_time, :time
+    field :weekdays, {:array, Ecto.Enum}, values: [:sun, :tue, :mon, :web, :thu, :fri, :sat]
 
     belongs_to :user, MaiquiRoutine.User
     belongs_to :category, MaiquiRoutine.Category
@@ -18,7 +39,7 @@ defmodule MaiquiRoutine.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:name, :start_time, :end_time])
-    |> validate_required([:name, :start_time, :end_time])
+    |> cast(attrs, @fields_that_can_be_changed)
+    |> validate_required(@required_fields)
   end
 end
