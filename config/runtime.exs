dc37a844429037285c92cd12962493edefdcc485
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-29 20:31:39 -0300

    :rocket: set elixir and erlang version to deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -60,7 +60,7 @@ if config_env() == :prod do
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
   #
-  #     config :maiqui_routine, MaiquiRoutineWeb.Endpoint, server: true
+  config :maiqui_routine, MaiquiRoutineWeb.Endpoint, server: true
   #
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
