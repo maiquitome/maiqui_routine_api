515b17b303073cfd491cee2997209e2853851eea
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-30 11:47:48 -0300

    :seedling: add maiqui2 user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -43,3 +43,37 @@ task_params = %{
 }
 
 {:ok,  %Task{id: _task_id}} = Tasks.create task_params
+
+###################
+##### Maiqui2 User
+###################
+
+alias MaiquiRoutine.{Category, Categories, Task, Tasks, User, Users}
+
+# CREATE USER
+user_params = %{name: "Maiqui2", email: "maiqui2@email.com", password: "12345678"}
+
+{:ok,  %User{id: user_id}} = Users.create user_params
+
+# CREATE CATEGORY
+category_params = %{
+  user_id: user_id,
+  name: "study",
+  dark_color: "2F9CF3",
+  light_color: "BBDEFB",
+  title_color: "456170"
+}
+
+{:ok,  %Category{id: category_id}} = Categories.create category_params
+
+# CREATE TASK
+task_params = %{
+  name: "english",
+  start_time: ~T[05:30:00.000],
+  end_time: ~T[06:00:00.000],
+  category_id: category_id,
+  user_id: user_id,
+  weekdays: [:sun, :tue, :mon]
+}
+
+{:ok,  %Task{id: _task_id}} = Tasks.create task_params
