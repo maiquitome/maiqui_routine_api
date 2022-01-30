af4ccdaa10bc8be0a64c9bb987064abc362154af
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-30 11:43:17 -0300

    :sparkles: add get_all_categories_by_user_id

--- lib/maiqui_routine/categories.ex
+++ lib/maiqui_routine/categories.ex
@@ -1,6 +1,7 @@
 defmodule MaiquiRoutine.Categories do
-  alias MaiquiRoutine.Categories.{Create, GetByName}
+  alias MaiquiRoutine.Categories.{Create, GetByName, GetAllByUserId}
 
   defdelegate create(params), to: Create, as: :call
   defdelegate get_by_name(category_name, user_id), to: GetByName, as: :call
+  defdelegate get_all_by_user_id(user_id), to: GetAllByUserId, as: :call
 end
---
+++ lib/maiqui_routine/categories/get_all_by_user_id.ex
@@ -0,0 +1,30 @@
+defmodule MaiquiRoutine.Categories.GetAllByUserId do
+  import Ecto.Query
+
+  alias MaiquiRoutine.{Category, Repo, User, Users}
+  alias Ecto.Schema
+
+  @doc """
+  ### Examples
+
+      iex> alias MaiquiRoutine.{User, Users, Category, Categories}
+
+      iex> {:ok, %User{id: user_id}} = Users.get_by_email "maiqui@email.com"
+
+      iex> Categories.GetAll.call user_id
+      {:ok, [%Category{}]}
+
+      iex> Categories.GetAll.call "ad42bac1-5cae-4c7c-8b40-8f056d5163a1"
+      {:error, "User not found"}
+
+  """
+  @spec call(binary()) :: {:ok, [Schema.t()]}
+  def call(user_id) do
+    query = from(Category, where: [user_id: ^user_id])
+
+    with {:ok, %User{}} <- Users.get_by_id(user_id),
+         categories <- Repo.all(query) do
+      {:ok, categories}
+    end
+  end
+end
