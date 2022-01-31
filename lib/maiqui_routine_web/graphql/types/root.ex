8149fe875109d994cc5b84cd677a2c5d03e632e9
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-31 20:38:21 -0300

    :sparkles: add error payload

--- lib/maiqui_routine_web/graphql/types/root.ex
+++ lib/maiqui_routine_web/graphql/types/root.ex
@@ -9,6 +9,12 @@ defmodule MaiquiRoutineWeb.Graphql.Types.Root do
 
   import_types MaiquiRoutineWeb.Graphql.Types
 
+  import AbsintheErrorPayload.Payload
+  import_types AbsintheErrorPayload.ValidationMessageTypes
+
+  payload_object(:user_payload, :user)
+  payload_object(:category_payload, :category)
+
   object :root_query do
     @desc "Get user by id"
     field :user, type: :user do
@@ -42,18 +48,20 @@ defmodule MaiquiRoutineWeb.Graphql.Types.Root do
 
   object :root_mutation do
     @desc "Creates a new user"
-    field :create_user, type: :user do
+    field :create_user, type: :user_payload do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware(&build_payload/2)
       middleware Log
     end
 
     @desc "Creates a new category"
-    field :create_category, type: :category do
+    field :create_category, type: :category_payload do
       arg :input, non_null(:create_category_input)
 
       resolve &CategoryResolver.create/2
+      middleware(&build_payload/2)
       middleware Log
     end
   end
