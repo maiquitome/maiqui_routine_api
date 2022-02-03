28b6e207e5af1a2c6c04c4a5c112781c782ef288
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-03 20:54:45 -0300

    :recycle: remove colour fields and add colour_palette_id

--- lib/maiqui_routine_web/graphql/types/category.ex
+++ lib/maiqui_routine_web/graphql/types/category.ex
@@ -3,18 +3,14 @@ defmodule MaiquiRoutineWeb.Graphql.Types.Category do
 
   object :category do
     field :id, non_null(:uuid4)
-    field :dark_color, non_null(:string)
-    field :light_color, non_null(:string)
     field :name, non_null(:string)
-    field :title_color, non_null(:string)
     field :user_id, non_null(:uuid4)
+    field :colour_palette_id, non_null(:uuid4)
   end
 
   input_object :create_category_input do
-    field :dark_color, non_null(:string), description: "Example: 2F9CF3"
-    field :light_color, non_null(:string), description: "Example: BBDEFB"
     field :name, non_null(:string)
-    field :title_color, non_null(:string), description: "Example: 456170"
     field :user_id, non_null(:uuid4)
+    field :colour_palette_id, non_null(:uuid4)
   end
 end
