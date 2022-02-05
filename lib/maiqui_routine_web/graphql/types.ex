72d45eaf6f2100c1eb63622078bfcdaf2b4ef717
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-04 23:22:35 -0300

    :sparkles: add graphql type task and custom

--- lib/maiqui_routine_web/graphql/types.ex
+++ lib/maiqui_routine_web/graphql/types.ex
@@ -3,7 +3,9 @@ defmodule MaiquiRoutineWeb.Graphql.Types do
 
   alias MaiquiRoutineWeb.Graphql.Types
 
-  import_types Types.User
+  import_types Absinthe.Type.Custom
   import_types Types.Category
   import_types Types.Custom.UUID4
+  import_types Types.Task
+  import_types Types.User
 end
