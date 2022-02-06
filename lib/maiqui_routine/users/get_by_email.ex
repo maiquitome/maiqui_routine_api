6079652633b62e19a8c494f9b9fc545bb5a48f73
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-06 14:27:51 -0300

    :recycle: change error message

--- lib/maiqui_routine/users/get_by_email.ex
+++ lib/maiqui_routine/users/get_by_email.ex
@@ -9,18 +9,18 @@ defmodule MaiquiRoutine.Users.GetByEmail do
       {:ok, %MaiquiRoutine.User{}}
 
       iex> MaiquiRoutine.Users.GetByEmail.call "maiqui@email"
-      {:error, "User not found"}
+      {:error, "User not found. Please check email."}
 
   """
   @spec call(binary()) :: {:ok, Schema.t()} | {:error, String.t()}
   def call(email) when is_binary(email) do
     case Repo.get_by(User, email: email) do
-      nil -> {:error, "User not found"}
+      nil -> {:error, "User not found. Please check email."}
       user_schema -> {:ok, user_schema}
     end
   end
 
   def call(_) do
-    {:error, "email must be of type binary"}
+    {:error, "Email must be of type binary."}
   end
 end
